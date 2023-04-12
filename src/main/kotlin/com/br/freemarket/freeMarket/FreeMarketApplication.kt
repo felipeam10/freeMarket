56316e2629ca6ab6e0421e869ad000c0037e99fd
felipeam10 <felipeam10@hotmail.com>
2023-04-12 11:01:39 -0300

    first commit

---
+++ src/main/kotlin/com/br/freemarket/freeMarket/FreeMarketApplication.kt
@@ -0,0 +1,11 @@
+package com.br.freemarket.freeMarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FreeMarketApplication
+
+fun main(args: Array<String>) {
+	runApplication<FreeMarketApplication>(*args)
+}
---
+++ src/test/kotlin/com/br/freemarket/freeMarket/FreeMarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.br.freemarket.freeMarket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FreeMarketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
