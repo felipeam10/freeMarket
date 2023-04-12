930406294645c753aa1d8571e79ddb34acdf82a0
felipeam10 <felipeam10@hotmail.com>
2023-04-12 11:55:45 -0300

    alterando o path do endpoint

--- src/main/kotlin/com/br/freemarket/freeMarket/controller/CustumerController.kt
+++ src/main/kotlin/com/br/freemarket/freeMarket/controller/CustumerController.kt
@@ -1,13 +1,20 @@
 package com.br.freemarket.freeMarket.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("customer")
 class CustumerController {
 
     @GetMapping
     fun helloWorld(): String {
-        return "Hello World do Controller"
+        return "Hello World do Controller com customer"
+    }
+
+    @GetMapping("/2")
+    fun customer(): String {
+        return "Hello World do Controller com customer 2"
     }
 }
